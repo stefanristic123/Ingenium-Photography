689bea2adb9ed59142e75e43329c7621becdc0d9
stefanristic123 <titanium955@gmail.com>
2019-04-06 12:38:56 +0200

    first push

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Animate Smoooth scroll
+
+$('#view-work').on('click', function(){
+    const images = $('images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
